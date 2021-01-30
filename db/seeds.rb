153fa2f09250bd8b57ecdc796a3a405282f2ac01
Josephine Bromley <bromleyjosephine@gmail.com>
2021-01-30 12:15:56 +0000

    created Ingredients seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# Creating ingredients database
+require 'open-uri'
+require 'json'
+
+puts "Creating ingredients"
+
+ingredients_api = JSON.parse(open("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list").read)
+ingredients_list = ingredients_api["drinks"]
+ingredients_list.map do |ingredient|
+  my_ingredient = Ingredient.create(name: ingredient["strIngredient1"])
+  puts "Ingredient #{my_ingredient.id} has been created"
+end
+
+puts "Done"
